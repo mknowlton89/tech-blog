6610aa5d68015451bff4bf4b7f0efb2e724965de
Michael Knowlton <mknowlton89@gmail.com>
2021-04-26 13:15:52 -0400

    Got all of my major routes setup.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,22 @@
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-    res.render('test')
+    res.render('home')
 });
 
+router.get('/dashboard', async (req, res) => {
+    res.render('dashboard')
+});
+
+router.get('/login', async (req, res) => {
+    res.render('login')
+});
+
+router.get('/logout', async (req, res) => {
+    res.render('logout')
+});
+
+router.get('/signup', async (req, res) => {
+    res.render('signup')
+});
 module.exports = router;
