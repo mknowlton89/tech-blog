1f7ee4f9487bcc76a3af8fb98cc9089a943822b0
Michael Knowlton <mknowlton89@gmail.com>
2021-04-26 15:43:05 -0400

    got sequelize setup so when the server is started, it creates the tables.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+
 
 router.get('/', async (req, res) => {
     res.render('home')
---
+++ public/script.js
@@ -0,0 +1,21 @@
+//JS Variables
+const signupForm = document.getElementById('signup-form');
+
+// DOM Variables
+
+
+// Function Definitions
+function createNewUser(e) {
+    event.preventDefault();
+    alert("Signup button was clicked!")
+};
+
+function init() {
+    alert('Page was loaded!')
+};
+
+
+// Event Listeners
+init();
+
+form.addEventListener('submit', createNewUser(event))
