7ef1eb677a9191fddab557355aeb1d2986f9545b
Michael Knowlton <mknowlton89@gmail.com>
2021-04-26 09:36:48 -0400

    Got the main handlebars layout working on the / route with the test.handlebars layout correctly being embedded within the main.handlebars template.

--- server.js
+++ server.js
@@ -5,20 +5,21 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 // Import the custom helper methods
-// const helpers = require('./utils/helpers');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Incorporate the custom helper methods
-// const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({ helpers });
 
-// app.engine('handlebars', hbs.engine);
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/home-routes'));
 
 app.use(routes);
 
